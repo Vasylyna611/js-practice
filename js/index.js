08ef3b7a122442971df8ecde633c73551ce3b726
Vasylyna611 <vasylynakuzyshyn@gmail.com>
2021-03-17 15:44:07 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
 function litres(time) {
-  return Math.floor(Math.floor(time) * 0.5);
+  return Math.floor(time * 0.5);
 }
 //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet (name, owner) {
@@ -12,7 +12,7 @@ function greet (name, owner) {
 }
 //https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
 function move (position, roll) {
