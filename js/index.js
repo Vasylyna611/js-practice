69a01c419d21253204f2cb75a5a8fe0a61d1cacb
Vasylyna611 <vasylynakuzyshyn@gmail.com>
2021-03-17 15:00:51 +0000

    Samaple code

--- js/index.js
+++ js/index.js
@@ -12,5 +12,9 @@ function greet (name, owner) {
 }
 //https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals)
+  return (laLigaGoals + copaDelReyGoals + championsLeagueGoals);
+}
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move (position, roll) {
+  return position + roll * 2;
 }
